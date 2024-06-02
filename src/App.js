07086141edf4f69d4b4d7f50fa73674ec3df1d45
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-06-02 17:22:18 +0900

    ロード中の表示/非表示追加

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { getAllPokemon } from "./utils/pokemon.js";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  //ロード画面の表示/非表示を設定
+  const [loading, setLoading] = useState(true);
 
   //useEffectを使用して[](最初にレンダリング)されたときにデータの取得を実行する。
   useEffect(() => {
@@ -11,11 +13,21 @@ function App() {
       //全てのポケモンデータを取得しresに格納
       let res = await getAllPokemon(initialURL);
       console.log(res);
+      setLoading(false);
     };
     fetchPokemonData();
   }, []);
 
-  return <div className="App"></div>;
+  return <div className="App">
+    {/* trueの場合はロード中/falseの場合は取得しましたを返す */}
+    {loading ? (
+      <h1>ロード中・・・</h1>
+    ) : (
+      <>
+      <h1>ポケモンデータを取得しました。</h1>
+      </>
+    )}
+  </div>;
 }
 
 export default App;
