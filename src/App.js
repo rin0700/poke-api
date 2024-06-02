cbe5a3dd02d59d18434a6a3bf5f9409ff0ed5aec
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-06-02 16:26:51 +0900

    Apiからデータ取得

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import { getAllPokemon } from "./utils/pokemon.js";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
 
-  return (
-    <div className="App"></div>
-  );
+  //useEffectを使用して[](最初にレンダリング)されたときにデータの取得を実行する。
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      //全てのポケモンデータを取得しresに格納
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+    };
+    fetchPokemonData();
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
---
+++ src/utils/pokemon.js
@@ -0,0 +1,8 @@
+export const getAllPokemon = (url) => {
+  //Promise=非同期処理を実行
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => resolve(data));
+  });
+};
